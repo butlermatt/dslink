b07d110500f8ac4ec65103be7d61f8c46665b836
Matthew Butler <butler.matthew@gmail.com>
2017-07-21 14:55:25 -0300

    Complete logging package. Add many tests

--- log/log.go
+++ log/log.go
@@ -1,12 +1,15 @@
 package log
 
 import (
+	"fmt"
 	"io"
 	"os"
+	"sync"
+	"time"
 )
 
-// TODO: Output should be like: Short Date and Time [RootPrefix][OtherPrefix]: log info
-
+// Level represents one of the levels of which logs may be stored at. If a log is called on a level
+// below the current level then it will be discarded.
 type Level int
 
 // String will provide a string representation of the log Level
@@ -42,47 +45,157 @@ const (
 	DisabledLevel
 )
 
-type Logger interface {
-	// Debug will attempt to log a DebugLevel message. If log level is set to a higher value, these will be discarded.
-	Debug(v ...interface{})
-	// Info will attempt to log an InfoLevel message. If log level is set to a higher value, these will be discarded.
-	Info(v ...interface{})
-	// Warn will attempt to log a WarningLevel message. If log level is set to a higher value, these will be discarded.
-	Warn(v ...interface{})
-	// Error will attempt to log an ErrorLevel message. This is the highest and most severe logging level. If log level
-	// is set to DisabledLevel, then these messages will be discarded.
-	Error(v ...interface{})
-}
+var (
+	currentLevel Level = WarningLevel
+	rootLogger   *Logger
+)
 
-type logger struct {
-	parent Logger
-	level  Level
+// Logger represents an active logging object that generates lines of output to an io.Writer.
+// Each logging operation makes a single call to the Writer's Write method. A Logger can be used
+// simultaneously from multiple goroutines. It guarantees to serialize access to the Writer.
+type Logger struct {
+	mu     sync.Mutex
+	parent *Logger
 	prefix string
+	buf    []byte
 	out    io.Writer
 }
 
-func (l *logger) Debug(v ...interface{}) {
+// Debug will attempt to log a DebugLevel message. If log level is set to a higher value, these will be discarded.
+func (l *Logger) Debug(v ...interface{}) {
+	if currentLevel > DebugLevel {
+		return
+	}
 
+	s := fmt.Sprintf(" %s", fmt.Sprint(v...))
+	l.writeBuf([]byte(s), DebugLevel)
 }
 
-func (l *logger) Info(v ...interface{}) {
+// Info will attempt to log an InfoLevel message. If log level is set to a higher value, these will be discarded.
+func (l *Logger) Info(v ...interface{}) {
+	if currentLevel > InfoLevel {
+		return
+	}
 
+	s := fmt.Sprintf(" %s", fmt.Sprint(v...))
+	l.writeBuf([]byte(s), InfoLevel)
 }
 
-func (l *logger) Warn(v ...interface{}) {
+// Warn will attempt to log a WarningLevel message. If log level is set to a higher value, these will be discarded.
+func (l *Logger) Warn(v ...interface{}) {
+	if currentLevel > WarningLevel {
+		return
+	}
 
+	s := fmt.Sprintf(" %s", fmt.Sprint(v...))
+	l.writeBuf([]byte(s), WarningLevel)
 }
 
-func (l *logger) Error(v ...interface{}) {
+// Error will attempt to log an ErrorLevel message. This is the highest and most severe logging level. If log level
+// is set to DisabledLevel, then these messages will be discarded.
+func (l *Logger) Error(v ...interface{}) {
+	if currentLevel > ErrorLevel {
+		return
+	}
 
+	s := fmt.Sprintf(" %s", fmt.Sprint(v...))
+	l.writeBuf([]byte(s), ErrorLevel)
 }
 
-var (
-	currentLevel Level = WarningLevel
-	rootLogger   Logger
-)
+func (l *Logger) write(lvl Level) {
+	if l.out == nil {
+		panic("logger has no output destination")
+	}
+
+	now := time.Now()
+	var b []byte
+	b = append(b, fmt.Sprintf("[%s] ", lvl)...)
+	b = append(b, now.Format(time.StampMilli)...)
+	b = append(b, ' ')
+	b = append(b, l.buf...)
+	if b[len(b)-1] != '\n' {
+		b = append(b, '\n')
+	}
+
+	l.out.Write(b)
+}
+
+func (l *Logger) writeBuf(b []byte, lvl Level) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.addPrefix()
+	l.buf = append(l.buf, b...)
+
+	if l.parent != nil {
+		l.parent.writeBuf(l.buf, lvl)
+	} else {
+		l.write(lvl)
+	}
+
+	l.buf = l.buf[:0]
+}
+
+func (l *Logger) addPrefix() {
+	if len(l.prefix) >= 1 {
+		l.buf = append(l.buf, '[')
+		l.buf = append(l.buf, l.prefix...)
+		l.buf = append(l.buf, ']')
+	}
+}
 
 func init() {
-	rootLogger = &logger{ nil, WarningLevel, "DSA", os.Stderr}
+	rootLogger = &Logger{
+		parent: nil,
+		prefix: "DSA",
+		out:    os.Stderr,
+	}
 }
 
+// SetLevel will specify the global logging level. Any logging calls below the level specified will be ignored.
+// The default Level is WarningLevel
+func SetLevel(level Level) {
+	currentLevel = level
+}
+
+// SetOutput will change the output Writer of all loggers to be the one specified. The logger will _not_
+// close the Writer and must be closed elsewhere. The default Output destination is os.Stderr
+func SetOutput(w io.Writer) {
+	rootLogger.out = w
+}
+
+// New will create a new logger, with the specified prefix. The prefix will be added the start of every message at
+// ever log level. You can also specify a parent logger, which will affix the prefix of the parent before the prefix
+// of the returned logger. If parent is `nil` then the parent will be the root DSA logger, and will prefix the message
+// messages at each log level with `[LogLevel] Date/Time Stamp [DSA]`
+func New(prefix string, parent *Logger) *Logger {
+	l := &Logger{prefix: prefix}
+	if parent == nil {
+		l.parent = rootLogger
+	} else {
+		l.parent = parent
+	}
+
+	return l
+}
+
+// Debug will attempt to log a DebugLevel message on the root logger. If log level is set to a higher value, these will be discarded.
+func Debug(v ...interface{})  {
+	rootLogger.Debug(v...)
+}
+
+// Info will attempt to log an InfoLevel message on the root logger. If log level is set to a higher value, these will be discarded.
+func Info(v ...interface{}) {
+	rootLogger.Info(v...)
+}
+
+// Warn will attempt to log a WarningLevel message on the root logger. If log level is set to a higher value, these will be discarded.
+func Warn(v ...interface{}) {
+	rootLogger.Warn(v...)
+}
+
+// Error will attempt to log an ErrorLevel message on the root logger. This is the highest and most severe logging level. If log level
+// is set to DisabledLevel, then these messages will be discarded.
+func Error(v ...interface{}) {
+	rootLogger.Error(v...)
+}
---
+++ log/log_test.go
@@ -0,0 +1,121 @@
+package log
+
+import (
+	"testing"
+	"bytes"
+	"strings"
+	"os"
+	"fmt"
+)
+
+
+func TestSetLevel(t *testing.T) {
+	if currentLevel != WarningLevel {
+		t.Errorf("Incorrect default level. expected=%q got=%q", WarningLevel, currentLevel)
+	}
+
+	SetLevel(DisabledLevel)
+
+	if currentLevel != DisabledLevel {
+		t.Errorf("SetLevel failed. expect=%q got=%q", DisabledLevel, currentLevel)
+	}
+}
+
+func TestSetOutput(t *testing.T) {
+	if rootLogger.out != os.Stderr {
+		t.Errorf("Incorrect default output. expect=%#v got=%#v", os.Stderr, rootLogger.out)
+	}
+
+	buf := new(bytes.Buffer)
+	SetOutput(buf)
+
+	if rootLogger.out != buf {
+		t.Errorf("SetOutput failed. expect=%#v got=%#v", buf, rootLogger.out)
+	}
+}
+
+func TestNew(t *testing.T) {
+	buf := new(bytes.Buffer)
+
+	SetOutput(buf)
+	SetLevel(DebugLevel)
+
+	log := New("Test", nil)
+	log.Debug("This is a test", 15)
+
+	str := buf.String()
+
+	if !strings.Contains(str, "[DSA]") {
+		t.Error("Logged line does not contain root prefix")
+	}
+
+	if !strings.Contains(str, "[Test]") {
+		t.Error("Logged line does not contain prefix.")
+	}
+
+	buf.Reset()
+
+	l2 := New("Test2", log)
+	l2.Debug("This is a test")
+
+	str = buf.String()
+
+	if !strings.Contains(str, "[DSA]") {
+		t.Error("Logged line does not contain root prefix")
+	}
+
+	if !strings.Contains(str, "[Test]") {
+		t.Error("Logged line does not contain parent prefix.")
+	}
+
+	if !strings.Contains(str, "[Test2]") {
+		t.Error("Logged line does not contain prefix")
+	}
+}
+
+func TestLogger_Levels(t *testing.T) {
+	l := New("Test", nil)
+	testLevels(t, DebugLevel, l.Debug, l.prefix)
+	testLevels(t, InfoLevel, l.Info, l.prefix)
+	testLevels(t, WarningLevel, l.Warn, l.prefix)
+	testLevels(t, ErrorLevel, l.Error, l.prefix)
+}
+
+func TestRootLevels(t *testing.T) {
+	testLevels(t, DebugLevel, Debug, "DSA")
+	testLevels(t, InfoLevel, Info, "DSA")
+	testLevels(t, WarningLevel, Warn, "DSA")
+	testLevels(t, ErrorLevel, Error, "DSA")
+}
+
+func testLevels(t *testing.T, l Level, f func(v ...interface{}), prefix string) {
+	levels := []Level{DebugLevel, InfoLevel, WarningLevel, ErrorLevel, DisabledLevel}
+	line := "This is a test"
+	buf := new(bytes.Buffer)
+	SetOutput(buf)
+
+	for _, lvl := range levels {
+		buf.Reset()
+		SetLevel(lvl)
+		f(line)
+		s := buf.String()
+		if lvl <= l {
+			if !strings.Contains(s, fmt.Sprintf("[%s]", l)) {
+				t.Errorf("Logged line does not contain level tag: %q, got %s", l, s)
+			}
+			if !strings.Contains(s, "[DSA]") {
+				t.Error("Logged line does not contain root prefix")
+			}
+			if !strings.Contains(s, fmt.Sprintf("[%s]", prefix)) {
+				t.Errorf("Logged line does not contain expected prefix: %q, got=%q", prefix, s)
+			}
+			if !strings.Contains(s, fmt.Sprintf("%s\n", line)) {
+				t.Errorf("Logged line does not contain provided. expected=%q got=%q", line, s)
+			}
+		} else {
+			if len(s) > 0 {
+				t.Errorf("Unexpected logged line at %q level: %s", lvl, s)
+			}
+		}
+	}
+}
