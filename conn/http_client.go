c6d7096f6b83d131d89add89396af3d5fb6bf1c6
Matthew Butler <butler.matthew@gmail.com>
2017-07-23 23:42:29 -0300

    Start with Dial of http client

--- conn/http_client.go
+++ conn/http_client.go
@@ -1,14 +1,41 @@
 package conn
 
 import (
-	"net/url"
 	"net/http"
+	"net/url"
 )
 
 import (
-	"github.com/butlermatt/dslink/crypto"
+	"fmt"
+	"strings"
 	"time"
+	"io/ioutil"
+	"encoding/json"
+	"github.com/gorilla/websocket"
+	"github.com/butlermatt/dslink/log"
+	"github.com/butlermatt/dslink/crypto"
 )
+
+type msgFormat int
+
+const (
+	fmtJson msgFormat = iota
+	fmtMsgP
+)
+
+type dsResp struct {
+	Id        string `json:"id"`
+	PublicKey string `json:"publicKey"`
+	WsUri     string `json:"wsUri"`
+	HttpUri   string `json:"httpUri"`
+	Version   string `json:"version"`
+	TempKey   string `json:"tempKey"`
+	Salt      string `json:"salt"`
+	SaltS     string `json:"saltS"`
+	SaltL     string `json:"saltL"`
+	Path      string `json:"path"`
+	Format    string `json:"format"`
+}
 
 type httpClient struct {
 	responder bool
@@ -19,7 +46,9 @@ type httpClient struct {
 	privKey   *crypto.PrivateKey
 	dsId      string
 	keyMaker  crypto.ECDH
+	format    msgFormat
 	htClient  *http.Client
+	wsClient  *websocket.Conn
 }
 
 func NewHttpClient(opts ...func(c *conf)) *httpClient {
@@ -57,4 +86,96 @@ func NewHttpClient(opts ...func(c *conf)) *httpClient {
 	}
 
 	return cl
+}
+
+func (cl *httpClient) Dial() error {
+	resp, err := cl.getWsConfig()
+	if err != nil {
+		return err
+	}
+
+	err = cl.connectWs(resp)
+
+	if err != nil {
+		return err
+	}
+
+	// Set timeouts and then handle connections
+
+	return nil
+}
+
+func (cl *httpClient) getWsConfig() (*dsResp, error) {
+	u, _ := url.Parse(cl.rawUrl.String()) // copy url
+	q := u.Query()
+	q.Add("dsId", cl.dsId)
+	if cl.tHash != "" {
+		q.Add("token", cl.token+cl.tHash)
+	}
+	u.RawQuery = q.Encode()
+
+	values := fmt.Sprintf("{\"publicKey\": \"%s\", \"isRequester\": %t, \"isResponder\": %t,"+
+		"\"linkData\": {}, \"version\": \"1.1.2\", \"formats\": [\"msgpack\",\"json\"], "+
+		"\"enableWebSocketCompression\": true}",
+		cl.privKey.PublicKey.Base64(), cl.requester, cl.responder)
+
+	res, err := cl.htClient.Post(u.String(), "application/json", strings.NewReader(values))
+	if err != nil {
+		return nil, fmt.Errorf("Error connecting to address: \"%s\"\nError: %s", cl.rawUrl, err)
+	}
+	defer res.Body.Close()
+
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to read response: %s", err)
+	}
+
+	dr := &dsResp{}
+	if err = json.Unmarshal(b, dr); err != nil {
+		return nil, fmt.Errorf("Unable to decode response: %s\nError: %s", b, err)
+	}
+	log.Debug(fmt.Sprintf("Received configuration: %+v\n", *dr))
+	return dr, nil
+}
+
+func (cl *httpClient) connectWs(conf *dsResp) error {
+	switch conf.Format {
+	case "json":
+		cl.format = fmtJson
+	case "msgpack":
+		cl.format = fmtMsgP
+	default:
+		return fmt.Errorf("unknown message format %q", conf.Format)
+	}
+
+	pubKey, err := cl.keyMaker.UnmarshalPublic(conf.TempKey)
+	if err != nil {
+		return fmt.Errorf("unable to parse server key %q, Error: %v", conf.TempKey, err)
+	}
+
+	shared := cl.keyMaker.GenerateSharedSecret(*cl.privKey, pubKey)
+	auth := cl.keyMaker.HashSalt(conf.Salt, shared)
+
+	u, err := url.Parse(conf.WsUri)
+	if err != nil {
+		return fmt.Errorf("unable to parse websocket url %q, error: %v", conf.WsUri, err)
+	}
+
+	q := u.Query()
+	q.Add("auth", auth)
+	q.Add("format", conf.Format)
+	q.Add("dsId", cl.dsId)
+	if cl.tHash != "" {
+		q.Add("token", cl.token+cl.tHash)
+	}
+	u.RawQuery = q.Encode()
+	u.Scheme = "ws"
+
+	con, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		return fmt.Errorf("unable to connect to websocket at %q, error: %v", conf.WsUri, err)
+	}
+
+	cl.wsClient = con
+	return nil
 }
